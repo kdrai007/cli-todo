ab822ed67cfebd4a1889d4b9f0c91469d99c8695
kundan rai <987kundanrai@gmail.com>
2024-05-28 01:21:29 +0530

    inital commit

---
+++ ptodo/cli.py
@@ -0,0 +1,63 @@
+"""This module provides the RP To-Do CLI."""
+# ptodo/cli.py
+
+from pathlib import Path
+from typing import Optional
+
+import typer
+
+from ptodo import __app_name__, __version__, ERRORS, config, database
+
+app = typer.Typer()
+
+
+@app.command()
+def init(
+        db_path: str = typer.Option(
+            str(database.DEFAULT_DB_FILE_PATH),
+            "--db-path",
+            "-db",
+            prompt="todo database location?"
+        )
+
+) -> None:
+    """Initialize the todo database."""
+    app_init_error = config.init_app(db_path)
+    if app_init_error:
+        typer.secho(
+            f"Creating config file failed with {ERRORS[app_init_error]}",
+            fg=typer.colors.RED
+        )
+        raise typer.Exit(1)
+    db_init_error = database.init_database(Path(db_path))
+    if db_init_error:
+        typer.secho(
+            f"Creating database file failed with {ERRORS[db_init_error]}",
+            fg=typer.colors.RED
+        )
+        raise typer.Exit(1)
+    else:
+        typer.secho(
+            f"The todo database location is {db_path}",
+            fg=typer.colors.GREEN
+        )
+
+
+def _version_callback(value: bool) -> None:
+    if value:
+        typer.echo(f"{__app_name__} v{__version__}")
+        raise typer.Exit()
+
+
+@app.callback()
+def main(
+    version: Optional[bool] = typer.Option(
+        None,
+        "--version",
+        "-v",
+        help="Show the application's version and exit.",
+        callback=_version_callback,
+        is_eager=True,
+    )
+) -> None:
+    return
---
+++ tests/test_ptodo.py
@@ -0,0 +1,13 @@
+# tests/test_rptodo.py
+
+from ptodo import __app_name__, __version__, cli
+from typer.testing import CliRunner
+
+
+runner = CliRunner()
+
+
+def test_version():
+    result = runner.invoke(cli.app, ["--version"])
+    assert result.exit_code == 0
+    assert f"{__app_name__} v{__version__}\n" in result.stdout
